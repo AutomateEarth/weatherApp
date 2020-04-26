1e2949021390e242a5583fed0f30ab3a2222ecd2
Jim <jpstrategy_clr@outlook.com>
2020-04-25 21:18:23 -0400

    Integrate two APIs

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=daee8029d5db056020599978117e83cb&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=daee8029d5db056020599978117e83cb&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current)
+
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees outside, and it feels like ' + response.body.current.feelslike + '.')
 })
